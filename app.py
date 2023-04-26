8080547619928586da6bc14b8dcc46ccfc1fdebb
Chris Howes <chris@beacon.dev>
2023-04-26 16:03:11 -0400

    modify app

--- app.py
+++ app.py
@@ -4,10 +4,3 @@ from langchain.llms import OpenAI
 llm = OpenAI(openai_api_key=st.secrets['openai_api_key'])
 
 name = st.text_input("What is your name?")
-goal = st.text_area("What is your goal for the week?")
-
-response = st.text("Hello!")
-
-# response = llm("Hello!")
-
-# st.write(response)
